5c0473ca27d352b895739c4dfd1dfcede333ff5b
Mike Huls <m.huls@datanext.nl>
2024-11-09 10:02:00 +0100

    test - addded failing dependency test

--- test/testcmd.sh
+++ test/testcmd.sh
@@ -1,4 +1,6 @@
 uv run coverage run --omit="test/*" -m pytest -v --log-cli-level=DEBUG
+
+uv run coverage run --omit="test/*" -m pytest -v "test/" --log-cli-level=DEBUG
 uv run coverage report
 uv run coverage html
 start chrome %cd%/htmlcov/index.html
